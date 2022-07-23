0f832c4e178f9637930e6a1f59ebb10388b02f52
NotStatilko <valerastatilko@gmail.com>
2022-07-23 18:31:10 +0300

    Add download_url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description = 'A simple app for generating urlsafe_b64 encoded scrypt keys',
     author_email = 'thenonproton@pm.me',
     url = 'https://github.com/NotStatilko/cmdscrypt',
-    download_url = '',
+    download_url = 'https://github.com/NotStatilko/cmdscrypt/archive/refs/tags/v1.0.tar.gz',
     entry_points = '''
         [console_scripts]
         cmdscrypt=cmdscrypt:app
